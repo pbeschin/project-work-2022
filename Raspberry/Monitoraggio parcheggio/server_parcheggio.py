a0221b79b2a47c80d86a24329ae5c0590423585c
alvise.battistich <alvise.battistich@stud.tecnicosuperiorekennedy.it>
2022-06-23 09:18:05 +0200

    updated rasp

--- Raspberry/Monitoraggio parcheggio/server_parcheggio.py
+++ Raspberry/Monitoraggio parcheggio/server_parcheggio.py
@@ -1,12 +1,15 @@
 import paho.mqtt.client as mqtt
 import time
-import datetime
+from datetime import datetime, timezone, timedelta
 import json
 import os
 #import pyodbc
 import requests
 
 from display_park import *
+
+# delete retained message:
+# mosquitto_pub -h test.mosquitto.org -t pw2k22/1.1/uscita -n -r -d
 
 BASE_API_URL = "https://pw2022-apinode.azurewebsites.net/"
 
@@ -33,55 +36,80 @@ def printParkingSpots(level):
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    #"{root:PW2k22}/{version:}1.0/{level:0+}/{parkID:1+}"
-    #"pw2k22/1.0/0/1"
-    client.subscribe(f"{TOPIC_ROOT}/#")
+    client.subscribe(f"{TOPIC_ROOT}/{TOPIC_VERSION}/#")
 
 # The callback for when a PUBLISH message is received from the server.
+
+def update_park_api(espID: str, statoEsp: bool):
+    parkUrl = f"https://pw2022-apinode.azurewebsites.net/stato/{espID}"
+    #PUT /stato/:idEsp `{"statoEsp":Boolean}`
+    putObj = {"statoEsp": statoEsp}
+    print(parkUrl, putObj)
+    response = requests.put(parkUrl, json = putObj)
+    print(response.status_code, response.text)
 
 def on_park_update(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     [root, version, endpoint, level, parkID] = str(msg.topic).split("/")
     #print(tail)
     #print(f"level:{level},parkID:{parkID}")
+    parkingSpots[int(level)][int(parkID) - 1] = int(msg.payload)
+    espID = f"{level}{int(parkID):02d}"
 
-    if parkingSpots[int(level)][int(parkID) - 1] != int(msg.payload):
-        parkingSpots[int(level)][int(parkID) - 1] = int(msg.payload)
-        #WriteDB(espID[-3:], int(msg.payload));
+    statoEsp = (bool(msg.payload.decode("utf-8")))
+
+    update_park_api(espID, statoEsp)
+        
     printParkingSpots(int(level))
+
+def park_entry_api(RFID: str):
+    entryUrl = f"https://pw2022-apinode.azurewebsites.net/transazioni/{RFID}"
+    postObj = {"data_entrata" : str(datetime.now(timezone(timedelta(hours = 2))))}
+    print(entryUrl, postObj)
+    response = requests.post(entryUrl, json = postObj)
+    print(response.status_code, response.text)
 
 def on_park_entry(client, userdata, msg):
     print("entry")
     print(msg.topic+" "+str(msg.payload))
     [root, version, endpoint] = str(msg.topic).split("/")
-    RFID = "AAAAAAAAAAAAAAAAAAAA"
-    entryUrl = f"{BASE_API_URL}/transizioni/{RFID}"
-    postObj = {"DATA_ENTRATA" : str(datetime.datetime.now())}
-    response = requests.post(entryUrl, json = postObj)
-    print(response)
+    RFID = msg.payload.decode("utf-8")
+    park_entry_api(RFID)
 
 
+def park_exit_api(RFID: str):
+    exitUrl = f"https://pw2022-apinode.azurewebsites.net/transazioni/uscita/{RFID}"
+    putObj = {"data_uscita" : str(datetime.now(timezone(timedelta(hours = 2))))}
+    print(putObj)
+    response = requests.put(exitUrl, json = putObj)
+    print(response.status_code, response.text)
 
 def on_park_exit(client, userdata, msg):
     print("exit")
     print(msg.topic+" "+str(msg.payload))
     [root, version, endpoint] = str(msg.topic).split("/")
+    RFID = msg.payload.decode("utf-8")
+    park_exit_api(RFID)
     
 
 def on_message(client, userdata, msg):
+    matched = false
     try:
         [root, version, endpoint, *more] = str(msg.topic).split("/")
         if version != TOPIC_VERSION:
             raise Exception("Wrong version")
         if endpoint == "parcheggio":
             on_park_update(client, userdata, msg)
+            matched = true
         if endpoint == "entrata":
             on_park_entry(client, userdata, msg)
+            matched = true
         if endpoint == "uscita":
             on_park_exit(client, userdata, msg)
+            matched = true
+        
+        if matched == false:
+            print("on_message: 'endpoint didn't match any value'")
     except Exception as e:
         print(e)
     finally:
@@ -108,15 +136,10 @@ client.connect(MQTT_SERVER, MQTT_PORT, MQTT_TIMEOUT)
 
 alive = 1
 def updateDisplay():
-    start = time.perf_counter()
-    level = 0
     while alive:
-        now = time.perf_counter()
-        if (now - start) > 5:
-            start = now
-            level = not level
-        displayPark(0, len(parkingSpots[0]) - sum(parkingSpots[0]), display = 0)
-        displayPark(1, len(parkingSpots[1]) - sum(parkingSpots[1]), display = 1)
+        for level in range(2):
+            displayPark(level, len(parkingSpots[level]) - sum(parkingSpots[level]), display = level)
+            displayPark(level, len(parkingSpots[level]) - sum(parkingSpots[level]), display = level)
 
 
 displayThread = Thread(target=updateDisplay)
